51288c24f20284af7b47ada50c35c0b606f59632
Manuel <manuel.rodriguezgonzalo@gmail.com>
2017-02-27 16:13:30 +0100

    setup file

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get -y install vlc
+sudo apt-get -y install python-pip
+sudo apt-get -y install g++
+sudo apt-get -y install git
+sudo apt-get -y install vim
+sudo apt-get -y install gdebi
+sudo apt-get -y install curl
+sudo apt-get -y install cmake
+sudo apt-get -y install build-essential
+sudo apt-get -y install ssh
+sudo apt-get -y install default-jre 
+sudo apt-get -y install default-jdk
+
+
+sudo apt-get -y install texlive-base texlive-bibtex-extra 
+sudo apt-get -y install texlive-fonts-recommended
+sudo apt-get -y install texlive-fonts-extra
+sudo apt-get -y install texlive-latex-base
+sudo apt-get -y install texlive-latex-extra
+sudo apt-get -y install texlive-latex-recommended
+sudo apt-get -y install texlive-science
